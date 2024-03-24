a7d6a328316f9bbeaf8b0b595eed00fc3cb0dfc5
VanChanh <vanchanh0730@gmai.com>
2024-03-24 21:28:08 +0700

    service, jwt

---
+++ registry/src/test/java/com/iuh/spring/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.spring.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user/src/test/java/com/iuh/spring/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.spring.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
